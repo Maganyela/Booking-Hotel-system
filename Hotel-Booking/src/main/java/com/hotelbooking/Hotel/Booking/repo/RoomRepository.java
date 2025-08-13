450d27afcf4e41b2768cb3288d954cf25cdf5098
Lethabo18 <lethaboemission23@gmail.com>
2025-08-13 21:22:56 +0200

    Done the entity,repository and dto classe

--- Hotel-Booking/src/main/java/com/hotelbooking/Hotel/Booking/repo/RoomRepository.java
+++ Hotel-Booking/src/main/java/com/hotelbooking/Hotel/Booking/repo/RoomRepository.java
@@ -1,4 +1,13 @@
 package com.hotelbooking.Hotel.Booking.repo;
 
-public interface RoomRepository {
+import com.hotelbooking.Hotel.Booking.entity.Room;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+
+import java.util.List;
+
+public interface RoomRepository extends JpaRepository<Room, Long> {
+
+    @Query("")
+    List<String> findDistinctRoomTypes();
 }
